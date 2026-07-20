4182f977c9a2ef9fa64147bd18d7702c95b37d6c
ShaderCloud <contact@shader.cloud>
2026-07-20 14:19:23 +0100

    - Fix documentation image path

--- docs/components/mdx.tsx
+++ docs/components/mdx.tsx
@@ -3,14 +3,31 @@ import type { MDXComponents } from 'mdx/types';
 import Zoom from "react-medium-image-zoom";
 import "react-medium-image-zoom/dist/styles.css";
 
+const basePath =
+  process.env.NODE_ENV === "production"
+    ? "/rbxts-react-clean-ui"
+    : "";
+
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
-    Image: (props) => (
-      <Zoom>
-        <img {...props} />
-      </Zoom>
-    ),
+    Image: (props: React.ComponentProps<"img">) => {
+      let src = props.src;
+
+      if (
+        typeof src === "string" &&
+        src.startsWith("/") &&
+        !src.startsWith(basePath + "/")
+      ) {
+        src = `${basePath}${src}`;
+      }
+
+      return (
+        <Zoom>
+          <img {...props} src={src} />
+        </Zoom>
+      );
+    },
     ...components,
   } satisfies MDXComponents;
 }
