42b4d9cbd6832e846736ed32ad238180f025ed1d
ShaderCloud <contact@shader.cloud>
2026-07-29 11:56:00 +0100

    - Added tabs documentation

--- Stories/Layout/Tabs.story.tsx
+++ Stories/Layout/Tabs.story.tsx
@@ -1,20 +1,20 @@
 import React from "@rbxts/react";
-import { Box, Tabs, Container, createStory, Row, Text, Scroller, FlexItem } from "@rbxts/react-clean-ui";
+import { Box, Tabs, Container, createStory, Text, Scroller } from "@rbxts/react-clean-ui";
 
 export = createStory((props) => (
     <Container
         width="75%"
-        center="50%">
+        center>
         <Box>
             <Tabs>
                 <Tabs.Tab>
-                    <Tabs.Title text="Short Tab" />
+                    <Tabs.Title text="Short Story" />
                     <Tabs.Content>
-                        <Text text="Something something" />
+                        <Text text="Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean." />
                     </Tabs.Content>
                 </Tabs.Tab>
                 <Tabs.Tab>
-                    <Tabs.Title text="Longer Text Tab" />
+                    <Tabs.Title text="Tab with Scroller" />
                     <Tabs.Content>
                         <Scroller height="200">
                             <Text text="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc," />
