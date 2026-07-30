d93fb4ced4ecc9f80efdae8b3c0adfac9b4f6b63
ShaderCloud <contact@shader.cloud>
2026-07-30 15:33:40 +0100

    - Fix open in markdown view in Docs

--- docs/lib/shared.ts
+++ docs/lib/shared.ts
@@ -1,7 +1,11 @@
 export const appName = 'React Clean UI';
-export const docsRoute = '/';
-export const docsImageRoute = '/og/docs';
-export const docsContentRoute = '/llms.mdx/docs';
+const basePath = process.env.NODE_ENV === "production"
+  ? "/rbxts-react-clean-ui"
+  : "";
+
+export const docsRoute =  `${basePath}/`;
+export const docsImageRoute =  `${basePath}/og/docs`;
+export const docsContentRoute = `${basePath}/llms.mdx/docs`;
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
